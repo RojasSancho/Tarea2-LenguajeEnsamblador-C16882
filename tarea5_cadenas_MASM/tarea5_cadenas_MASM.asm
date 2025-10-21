22f5cf9473f25e1da623ae035851d9003238fbcf
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2025-10-21 13:57:23 -0600

    comentarios

--- tarea5_cadenas_MASM/tarea5_cadenas_MASM.asm
+++ tarea5_cadenas_MASM/tarea5_cadenas_MASM.asm
@@ -10,28 +10,28 @@ INCLUDELIB deps\Irvine32.lib ; enlaza la biblioteca
 
     ; reserva espacio en memoria para la cadena invertida
     ; SIZEOF origen devuelve la cantidad de caracteres en la cadena 'origen'
-    ; DUP('') rellena SIZEOF origen caracteres con caracteres vac�os
+    ; DUP('') rellena SIZEOF origen caracteres con caracteres vacios
     destino BYTE SIZEOF origen DUP(?)
 
 .code
 main PROC
-    mov esi, OFFSET origen + SIZEOF origen - 2 ; apuntar al �ltimo car�cter de la cadena origen (antes del byte nulo)      
+    mov esi, OFFSET origen + SIZEOF origen - 2 ; apuntar al ultimo caracter de la cadena origen (antes del byte nulo)      
     mov edi, OFFSET destino ; apuntar al inicio de la cadena destino
     mov ecx, LENGTHOF origen - 1 ; establecer el contador de caracteres (excluyendo el byte nulo)
 
 reverse_loop:
     cmp ecx, 0
     je end_loop
-    mov al, [esi]           ; cargar el car�cter actual desde el final de la cadena origen   
-    mov [edi], al           ; almacenar el car�cter en la cadena destino
-    dec esi                 ; mover al car�cter anterior en la cadena origen
-    inc edi                 ; mover al siguiente car�cter en la cadena destino
+    mov al, [esi]           ; cargar el caracter actual desde el final de la cadena origen   
+    mov [edi], al           ; almacenar el caracter en la cadena destino
+    dec esi                 ; mover al caracter anterior en la cadena origen
+    inc edi                 ; mover al siguiente caracter en la cadena destino
     dec ecx                 ; decrementar el contador de caracteres
     jmp reverse_loop
 
 end_loop:
     mov byte ptr [edi], 0   ; agregar el byte nulo al final de la cadena destino
-    ; parte que se encarga de la impresi�n
+    ; parte que se encarga de la impresion
     mov edx, OFFSET destino ; apuntar a la cadena destino
     call WriteString
     call Crlf
