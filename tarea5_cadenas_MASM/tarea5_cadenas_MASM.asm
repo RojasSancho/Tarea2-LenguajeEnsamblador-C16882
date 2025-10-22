e2f1c8dbb29c343c2b16f04c166a5a5b81c7dde1
RojasSancho <hermes.rojassancho@ucr.ac.cr>
2025-10-21 20:11:00 -0600

    utilizando la instruccion loop

--- tarea5_cadenas_MASM/tarea5_cadenas_MASM.asm
+++ tarea5_cadenas_MASM/tarea5_cadenas_MASM.asm
@@ -1,5 +1,5 @@
 INCLUDE deps\Irvine32.inc                       ; incluir WriteString, Crlf, DumpRegs, DumpMem
-INCLUDELIB deps\Irvine32.lib                    ; enlazar la biblioteca
+INCLUDELIB deps\Irvine32.lib                ; enlazar la biblioteca
 
 .data
     ; cadena original terminada en byte nulo ('\0')
@@ -15,26 +15,22 @@ main PROC
     ; inicializacion de registros
     mov esi, OFFSET origen + SIZEOF origen - 2  ; apuntar al ultimo caracter de la cadena origen (antes del byte nulo)      
     mov edi, OFFSET destino                     ; apuntar al inicio de la cadena destino
-    mov ecx, LENGTHOF origen - 1                ; establecer el contador de caracteres (ignorando el byte nulo)
+    mov ecx, LENGTHOF origen - 1                ; numero de caracteres a copiar (ignorando el byte nulo)
 
 reverse_loop:
-    cmp ecx, 0
-    je end_loop
-    mov al, [esi]                               ; cargar el caracter actual desde el final de la cadena origen   
-    mov [edi], al                               ; almacenar el caracter en la cadena destino
-    dec esi                                     ; retroceder al caracter anterior en la cadena origen
-    inc edi                                     ; avanzar al siguiente caracter en la cadena destino
-    dec ecx                                     ; decrementar el contador de caracteres
-    jmp reverse_loop
+    mov al, [esi]                               ; cargar el caracter actual desde el final 
+    mov [edi], al                               ; copiarlo a la cadena destino
+    dec esi                                     ; retroceder en la cadena origen (ESI)
+    inc edi                                     ; avanzar en la cadena destino (EDI)
+    loop reverse_loop                           ; repetir hasta que ECX sea 0
 
-end_loop:
-    mov byte ptr [edi], 0                       ; finalizar la cadena destino con un byte nulo (0)
+    mov BYTE PTR [edi], 0                       ; finalizar la cadena destino con un byte nulo (0)
 
     ; parte que se encarga de la impresion de texto en consola
     mov edx, OFFSET destino                     ; cargar en EDX la dirección de 'destino'
     call WriteString                            ; imprimir en consola lo que apunte EDX
     call Crlf                                   ; salto de línea
-    call DumpRegs                               ; muestra el contenido de los registros (paradepuración)
+    call DumpRegs                               ; muestra el contenido de los registros (para depuración)
     exit          
 main ENDP
 
